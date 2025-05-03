0f2200f5aefe2e024689ed62ee9a0ceda81dd0a0
Muhammad Taha Raees <raeesmuhammadtaha@gmail.com>
2025-05-03 22:02:02 +0100

    login

--- app/page.tsx
+++ app/page.tsx
@@ -20,9 +20,6 @@ export default function Home() {
                   Get Started <ArrowRight className="ml-2 h-5 w-5" />
                 </Link>
               </Button>
-              <Button asChild size="lg" variant="outline" className="border-white text-white hover:bg-white/10">
-                <Link href="/login">Login</Link>
-              </Button>
             </div>
           </div>
         </div>
